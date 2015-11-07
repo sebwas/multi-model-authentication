f3acac87ec3010621ef490a5b18acbb0a168fd46
Sebastian Wasser <sebastian.wasser@gmail.com>
2015-11-07 13:34:09 +0100

    Added Auth facade

---
+++ src/SebWas/MultiModelAuthentication/Facades/Auth.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace SebWas\MultiModelAuthentication\Facades;
+
+class Auth extends Illuminate\Support\Facades\Facade {
+	/**
+	 * Get the registered name of the component.
+	 *
+	 * @return string
+	 */
+	protected static function getFacadeAccessor(){
+		return 'auth';
+	}
+}
